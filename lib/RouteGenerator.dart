b1dec1a3ad23838ebfa49809f7b06155b95347cf
Hirantha <hiranthapeiris404@gmail.com>
2020-10-14 18:31:38 +0530

    boilerplate

--- lib/RouteGenerator.dart
+++ lib/RouteGenerator.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mun_care_app/screens/Dashboard/Dashboard.dart';
+import 'package:mun_care_app/screens/Error/ErrorView.dart';
 
-class RouteGenerator{
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
 
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => Dashboard());
+      case '/error':
+        return MaterialPageRoute(
+            builder: (_) => ErrorView(errorMsg: "Direct Route"));
+      default:
+        return MaterialPageRoute(
+            builder: (_) => ErrorView(errorMsg: 'Default Route'));
+    }
+  }
 }
