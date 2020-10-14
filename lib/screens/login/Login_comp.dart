fec27c32c4c6d152307547fee9c6065cfc22cb9c
Nadeeka-max <you@example.com>
2020-10-14 20:50:50 +0530

    create Signin page

---
+++ lib/screens/login/Login_comp.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+
+        body: Padding(
+          padding: EdgeInsets.all(40),
+          child: ListView(
+            children: <Widget>[
+              Container(
+
+                child: Text('LogIn Now',
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Text('Please login to continue using our app',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                alignment: Alignment.center,
+                child: Text('Enter via Social Networks',
+                  style: TextStyle(color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    leftSection,
+                    rightSection
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(20),
+                child: Text('or login with Email',
+                  style: TextStyle(fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: new BorderRadius.circular(20.0)),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: new BorderRadius.circular(20.0)),
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              FlatButton(
+                onPressed: (){
+                  //forgot password screen
+                },
+                child: Text('Forgot Password',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                height: 50.0,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: RaisedButton(
+                  shape: StadiumBorder(),
+                  textColor: Colors.white,
+                  color: Colors.lightBlue,
+                  child: Text('LogIn'),
+                  onPressed: (){
+
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Text('Does not have account?'),
+                    FlatButton(
+                      textColor: Colors.lightBlueAccent,
+                      child: Text('SingUp',
+                        style: TextStyle(fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.lightBlue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
+final leftSection = new Container(
+  alignment: Alignment.center,
+  padding: EdgeInsets.fromLTRB(80, 10, 10, 10),
+  child: Image.asset(
+    "assets/hh.png",
+    width: 70.0,
+    height: 70,
+  ),
+);
+
+
+final rightSection = new Container(
+  alignment: Alignment.center,
+  padding: EdgeInsets.fromLTRB(10, 10, 80, 10),
+  child: Image.asset(
+    "assets/gg.png",
+    width: 70.0,
+    height: 70,
+  ),
+);
+
+
