9c02df2d069a6b5a1ffc2b8802673278fe145916
Hirantha <hiranthapeiris404@gmail.com>
2020-10-30 22:47:38 +0530

    Notifications Service - Get user notifications

--- lib/widgets/Bottom_nav.dart
+++ lib/widgets/Bottom_nav.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mun_care_app/helpers/Constants.dart';
-import 'package:mun_care_app/services/NotificationService.dart';
+
 
 class Bottom_nav extends StatelessWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
-  final NotificationService _notificationService = NotificationService();
+
   Bottom_nav({
     Key key,
     this.scaffoldKey
@@ -26,9 +26,7 @@ class Bottom_nav extends StatelessWidget {
         children: <Widget>[
           BottomNavItem(
             title: "Today",
-            svgScr: "assets/icons/calendar.svg",press: ()async{
-              //await _notificationService.sendAndRetrieveMessage();
-          },
+            svgScr: "assets/icons/calendar.svg",
           ),
           BottomNavItem(
             title: "Menu",
