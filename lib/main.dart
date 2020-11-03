c7728377647b3279334d960c2c3691d446061492
unknown <hnishadisathsarani@gmail.com>
2020-11-03 16:36:05 +0530

    dary added

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyAppState extends State<MyApp> {
           textTheme:
               Theme.of(context).textTheme.apply(displayColor: kTextColor),
         ),
-        initialRoute: '/sechClinics',
+        initialRoute: '/dailyRepo',
         onGenerateRoute: RouteGenerator.generateRoute,
       ),
     );
