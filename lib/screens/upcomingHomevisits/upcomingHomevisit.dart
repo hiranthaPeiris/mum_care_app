6a428c42af297feab27f9a7c48904be070f9ab70
unknown <hnishadisathsarani@gmail.com>
2020-11-14 23:50:49 +0530

    View upcomingHomeVisits added

--- lib/screens/upcomingHomevisits/upcomingHomevisit.dart
+++ lib/screens/upcomingHomevisits/upcomingHomevisit.dart
@@ -8,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mun_care_app/models/homeVisitModel.dart';
 import 'package:mun_care_app/models/UserM.dart';
+import 'package:flutter/foundation.dart';
 //import 'package:mun_care_app/models/user.dart';
 //import 'package:mun_care_app/services/AuthServices.dart';
 
@@ -15,8 +16,7 @@ var uuid = Uuid();
 
 class UpcomingHomeVisitFirebaseDemo extends StatefulWidget {
   final String appTitle = "UpComing Home Visits";
-//  final String title;
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   var useri = new UserM.get().userCredential.user.uid;
 
   @override
@@ -29,10 +29,6 @@ class _UpcomingHomeVisitFirebaseDemoState
   bool isEditing = false;
   bool textFieldVisibility = false;
   TimeOfDay now = TimeOfDay.now();
-
-  UpcomingHomeVisitFirebaseDemo currentHomeVisit;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Stream<UserM> get user {
@@ -268,362 +264,6 @@ class _UpcomingHomeVisitFirebaseDemoState
         ],
       ),
     );
-    /*Scaffold(
-      resizeToAvoidBottomPadding: false,
-      backgroundColor: Colors.lightBlue,
-      //appBar: AppBar(
-      //title: Text('Page 2' ),
-      //title: Text('Upcoming Home Visits'),
-      //),
-      body: ListView(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Upcoming Home Visits',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.transparent),
-              Spacer(),
-              Container(
-                alignment: Alignment.topRight,
-                // backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.add,
-                ),
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.add,
-                ),
-              ),
-              Positioned(
-                top: 70.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40.0),
-                        topRight: Radius.circular(40.0),
-                      ),
-                      color: Colors.white),
-                  height: MediaQuery.of(context).size.height - 10.0,
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              Positioned(
-                top: 100.0,
-                left: 40.0,
-                //child: Container(),
-                child: Container(
-                  width: 350.0,
-                  height: 500.0,
-                  color: Colors.transparent,
-                  padding: EdgeInsets.fromLTRB(19, 19, 0, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      textFieldVisibility
-                          ? Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Column(
-                                  children: <Widget>[],
-                                ),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                              ],
-                            )
-                          : Container(),
-                      Text("Hope your Visiting.Get Ready....",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          )),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Flexible(
-                        child: buildBody(context),
-                      ),
-                      Container(
-                        alignment: Alignment.bottomLeft,
-                        child: FlatButton(
-                            color: Colors.white,
-                            textColor: Colors.blue,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                                side: BorderSide(color: Colors.blue)),
-                            child: Container(
-                              /*decoration: BoxDecoration(
-                              boxShadow: [
-                                new BoxShadow(
-                                    color: Colors.black, blurRadius: 30.0),
-                              ],
-                            ),*/
-                              child: Text(
-                                "Back",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue),
-                              ),
-                            ),
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/sechHomeVisits');
-                            }),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );*/
-  }
-
-  getAllVisits() {
-    var useri = new UserM.get().userCredential.user.uid;
-    //String uid = user.uid;
-
-    //String fcmToken = await _auth.authStateChanges();
-    //user.uid = _auth.authStateChanges();
-    return FirebaseFirestore.instance
-        .collection('Bookings')
-        .doc('IhiVRXSUfZPKoKpaNZgFtlPosj22')
-        .collection('HomeVisits')
-        .snapshots();
-  }
-
-  //delete
-  deleteVisits(UpHomeVisit b1) {
-    FirebaseFirestore.instance.runTransaction((transaction) async {
-      // ignore: await_only_futures
-      await transaction.delete(b1.documentReference);
-    });
-  }
-
-  //delete
-  /* deleteVisit(Book book) {
-    FirebaseFirestore.instance.runTransaction((transaction) async {
-      // ignore: await_only_futures
-      await transaction.delete(book.documentReference);
-    });
-  }*/
-
-  Widget buildBody(BuildContext context) {
-    //  var user = new UserM.get();
-
-    return StreamBuilder<QuerySnapshot>(
-      stream: getAllVisits(),
-      // ignore: missing_return
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        // (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("Error ${snapshot.error}");
-        }
-        if (snapshot.hasData) {
-          print("Documents -> ${snapshot.data.docs.length}");
-          print("uid:${new UserM.get().userCredential.user.uid}");
-          return buildList(context, snapshot.data.docs);
-        }
-      },
-    );
-  }
-
-//Widget buliList(@override
-  Widget buildList(BuildContext context, List<QueryDocumentSnapshot> snapshot) {
-    return ListView(
-      children: snapshot.map((data) => listItemBuild(context, data)).toList(),
-    );
-  }
-
-  Widget listItemBuild(BuildContext context, DocumentSnapshot data) {
-    final visit = UpHomeVisit.fromSnapshot(data);
-    return Column(
-      children: <Widget>[
-        SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              InkWell(
-                onTap: () {
-                  openBottomSheet('sfsgs');
-                },
-                child: Stack(
-                  alignment: Alignment.center,
-                  overflow: Overflow.visible,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.all(11.0),
-                      height: 70.0,
-                      width: 300.0,
-                      decoration: BoxDecoration(
-                          color: Colors.blue[100],
-                          borderRadius: BorderRadius.circular(15.0)),
-                    ),
-                    Positioned(
-                      bottom: 20.0,
-                      child: Container(
-                        height: 50.0,
-                        width: 330.0,
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(15.0)),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 25.0,
-                      child: Container(
-                        // height: 80.0,
-                        width: 350.0,
-                        decoration: BoxDecoration(
-                          color: Colors.blue[50],
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                height: 60,
-                                width: 70,
-                                margin: const EdgeInsets.all(2.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    border: Border.all(
-                                      color: Colors.blueGrey,
-                                      width: 2,
-                                    ),
-                                    borderRadius: BorderRadius.circular(10.0)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  key: ValueKey(visit.dateTime),
-                                  child: Text(visit.dateTime.toString(),
-                                      textAlign: TextAlign.left),
-                                  // child: Text(snapshot.data
-                                  //   .documents[index]['dateTime']
-                                  // .toString(),textAlign: TextAlign.center
-                                  //),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Row(
-                                        children: <Widget>[
-                                          Spacer(),
-
-                                          /*Text(
-                                            "${now}",
-                                            
-                                            style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 12.0),*/
-                                          //new DateTime.now(),
-                                        ],
-                                      ),
-                                      Text(visit.description.toString(),
-                                          style: TextStyle(fontSize: 15),
-                                          textAlign: TextAlign.left),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(visit.dateTime.toString(),
-                                              textAlign: TextAlign.left),
-                                          Spacer(),
-                                          Icon(
-                                            Icons.star_border,
-                                            color: Colors.orange,
-                                            size: 20.0,
-                                          )
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
-    );
-    /*Padding(
-      key: ValueKey(visit.dateTime),
-      padding: EdgeInsets.symmetric(vertical: 19, horizontal: 1),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.blue),
-          borderRadius: BorderRadius.circular(4.0),
-        ),
-        child: SingleChildScrollView(
-          child: ListTile(
-            title: Column(
-              children: <Widget>[
-                Card(
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.alarm_add,
-                        color: Colors.blue,
-                      ),
-                      Text(visit.dateTime.toString()),
-                    ],
-                  ),
-                ),
-                Divider(),
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.messenger_rounded,
-                      color: Colors.blue,
-                    ),
-                    Text(visit.description.toString()),
-                  ],
-                ),
-              ],
-            ),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-              onPressed: () {
-                // deletewBook(book);
-                deleteVisits(visit);
-              },
-            ),
-            onTap: () {
-              //setUpdateUI(book);
-            },
-          ),
-        ),
-      ),
-    );*/
   }
 
   void openBottomSheet(String itemTitle) {
@@ -724,6 +364,55 @@ class _UpcomingHomeVisitFirebaseDemoState
                           ),
                         ),
                       ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 50.0,
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          alignment: Alignment.bottomLeft,
+                          child: RaisedButton(
+                            shape: StadiumBorder(),
+                            textColor: Colors.white,
+                            color: Colors.lightBlue,
+                            child: Text(
+                              'Deny',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            onPressed: () {
+                              Text('A HomeVisit Successfully Created');
+                              // Navigator.pushNamed(context, '/upcomingHomevisit');
+                              //addBook();
+                            },
+                          ),
+                        ),
+                        Container(
+                          height: 50.0,
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          alignment: Alignment.bottomRight,
+                          child: RaisedButton(
+                            shape: StadiumBorder(),
+                            textColor: Colors.white,
+                            color: Colors.lightBlue,
+                            child: Text(
+                              'Accept',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            onPressed: () {
+                              Text('A HomeVisit Successfully Created');
+                              Navigator.pushNamed(context, '/notification');
+                              return gt;
+                              //Navigator.push(context, route)
+
+                              //addBook();
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
