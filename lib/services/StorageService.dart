f85d60889cfae3c14ec5f705fddcd95f6be5c3f3
Hirantha <hiranthapeiris404@gmail.com>
2020-12-12 22:26:48 +0530

    storage and notification services

---
+++ lib/services/StorageService.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<dynamic> uploadProfileImage(File _image, String uid,String folder) async {
+
+    firebase_storage.Reference ref = storage.ref('$folder/').child(uid);
+
+    try {
+      firebase_storage.UploadTask task = ref.putFile(_image);
+      firebase_storage.TaskSnapshot snapshot = await task;
+      print('Uploaded ${snapshot.bytesTransferred} bytes.');
+    } on FirebaseException catch (e) {
+      print(e);
+
+      if (e.code == 'permission-denied') {
+        print('User does not have permission to upload to this reference.');
+      }
+      return e;
+    }
+  }
+
+  Future<String> downloadProfileImage(String folder, String uid) async {
+    // Directory appDocDir = await getApplicationDocumentsDirectory();
+    // File downloadToFile = File('${appDocDir.path}/mum-profile.png');
+    String url;
+    try {
+      url = await storage.ref('$folder/').child(uid).getDownloadURL();
+      print(url);
+      //firebase_storage.TaskSnapshot snapshot = await task;
+      //print('Uploaded ${snapshot.bytesTransferred} bytes.');
+      return url;
+    } on FirebaseException catch (e) {
+      print(e);
+
+      if (e.code == 'permission-denied') {
+        print('User does not have permission to upload to this reference.');
+      }
+      return null;
+    }
+  }
+}
